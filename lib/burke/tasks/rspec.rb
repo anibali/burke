951a920d06c548d5c5a8f98939d89c714ee335d5
Aiden Nibali <dismaldenizen@gmail.com>
2010-10-11 20:55:48 +1100

    update for RSpec 2

--- lib/burke/tasks/rspec.rb
+++ lib/burke/tasks/rspec.rb
@@ -3,54 +3,22 @@ module Burke
   
   define_task 'spec' do |s|
     begin
-      require 'spec/rake/spectask'
+      require 'rspec/core/rake_task'
       
       r = s.rspec
       opts = build_rspec_opts r
       
-      Spec::Rake::SpecTask.new 'spec' do |t|
-        t.spec_files = r.files
-        t.spec_opts = opts
+      RSpec::Core::RakeTask.new 'spec' do |t|
         t.ruby_opts = r.ruby_opts if r.ruby_opts
       end
     rescue LoadError
-    end unless s.rspec.files.empty?
-  end
-  
-  define_task 'spec:rcov' do |s|
-    begin
-      require 'spec/rake/spectask'
-      require 'rcov'
-      require 'spec/rake/verify_rcov'
-      
-      r = s.rspec
-      opts = build_rspec_opts r
-      
-      desc "Run specs with RCov"
-      Spec::Rake::SpecTask.new('spec:rcov') do |t|
-        t.spec_files = r.files
-        t.spec_opts = opts
-        t.rcov = true
-        t.rcov_opts = ['--exclude', 'spec']
-      end
-      
-      desc "Run specs with RCov and verify code coverage"
-      RCov::VerifyTask.new('spec:rcov:verify' => 'spec:rcov') do |t|
-        t.threshold = r.rcov.threshold
-        t.index_html = 'coverage/index.html'
-      end if r.rcov.threshold
-    rescue LoadError
-    end if task_enabled? :spec
+    end
   end
   
   def self.build_rspec_opts r
     opts = []
-    if r.options_file
-      opts << "--options" << r.options_file
-    else
-      opts << "--colour" if r.color
-      opts << "--format" << r.format if r.format
-    end
+    opts << "--colour" if r.color
+    opts << "--format" << r.format if r.format
     opts
   end
   
@@ -59,21 +27,8 @@ module Burke
   end
   
   class RSpecSettings < Holder
-    field 'files' do
-      Dir['spec/**/*_spec.rb'].freeze
-    end
-    
-    field 'options_file' do
-      find_file('{{spec/,}{spec.opts,.specopts}}').freeze
-    end
-    
-    field 'color' do
-      true
-    end
-    
-    field 'format' do
-      'progress'
-    end
+    field('color') { true }
+    field('format') { 'progress' }
     
     field 'ruby_opts'
     field('rcov') { self.rcov = RCovSettings.new }
